043577cbeee0458a0d04c695f6fc8e2f1f7b03af
chengq220 <chengqiu220@gmail.com>
2026-02-18 12:04:13 -0500

    update eval

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+pip install -e .
+
+cd checkpoints
+wget https://dl.fbaipublicfiles.com/segment_anything_2/072824/sam2_hiera_tiny.pt
+cd ..
+
+mkdir data
+cd data
+gdown --fuzzy "https://drive.google.com/file/d/1vkJbJ7JSQrq--Qze-E2_WL1xJeaLWsZx/view?usp=sharing"
+tar -xvf VOS-Endovis17.tar
+rm VOS-Endovis17.tar
+cd ..
