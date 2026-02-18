a9c8504c6ab65422628c72995708c4d84bd550a7
chengq220 <chengqiu220@gmail.com>
2026-02-18 17:29:42 -0500

    update format

--- evaluation.sh
+++ evaluation.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-python vos_inference.py \
-    --sam2_cfg configs/sam2.1/sam2_hiera_b+.yaml \
-    --sam2_checkpoint ./checkpoints/sam2.1_hiera_b+.pth \
-    --output_mask_dir ./results/sam2.1/endovis_2017 \
-    --input_mask_dir ./data/VOS-Endovis17/valid/VOS \
-    --base_video_dir ./data/VOS-Endovis17/valid/JPEGImages \
-    --gt_root ./data/VOS-Endovis2017/valid/Annotations \
+python tools/vos_inference.py \
+    --sam2_cfg "configs/sam2.1/sam2.1_hiera_b+.yaml" \
+    --sam2_checkpoint "./checkpoints/sam2.1_hiera_base_plus.pt" \
+    --output_mask_dir "./results/sam2.1/endovis_2017" \
+    --input_mask_dir "./data/VOS-Endovis17/valid/VOS" \
+    --base_video_dir "./data/VOS-Endovis17/valid/JPEGImages" \
+    --gt_root "./data/VOS-Endovis2017/valid/Annotations" \
     --gpu_id 0
