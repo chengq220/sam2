f6def27fff5c09c807913f1678a4f7d8bdf7ee05
chengq220 <chengqiu220@gmail.com>
2026-02-19 12:49:52 -0500

    update

--- setup.sh
+++ setup.sh
@@ -7,25 +7,25 @@ pip install scikit-image
 
 cd checkpoints
 wget https://dl.fbaipublicfiles.com/segment_anything_2/092824/sam2.1_hiera_base_plus.pt
-wget https://dl.fbaipublicfiles.com/segment_anything_2/092824/sam2.1_hiera_small.pt
+# wget https://dl.fbaipublicfiles.com/segment_anything_2/092824/sam2.1_hiera_small.pt
 cd ..
 
 mkdir data
 cd data
-gdown --fuzzy "https://drive.google.com/file/d/1vkJbJ7JSQrq--Qze-E2_WL1xJeaLWsZx/view?usp=sharing"
-tar -xvf endovis17.tar
-rm endovis17.tar
-cd VOS-Endovis17/valid
-gdown --fuzzy "https://drive.google.com/file/d/1FOdReaUUfceEvl-4mwZRsyg8qBdjjKZ1/view?usp=sharing"
-tar -xvf VOS-endovis17.tar
-rm VOS-endovis17.tar
-cd ..
+# gdown --fuzzy "https://drive.google.com/file/d/1vkJbJ7JSQrq--Qze-E2_WL1xJeaLWsZx/view?usp=sharing"
+# tar -xvf endovis17.tar
+# rm endovis17.tar
+# cd VOS-Endovis17/valid
+# gdown --fuzzy "https://drive.google.com/file/d/1FOdReaUUfceEvl-4mwZRsyg8qBdjjKZ1/view?usp=sharing"
+# tar -xvf VOS-endovis17.tar
+# rm VOS-endovis17.tar 
+# cd ..
 
-gdown --fuzzy "https://drive.google.com/file/d/1VojHbfTz9pgpZ3-7O_YcVxM6hybVDhY4/view?usp=sharing"
-tar -xvf endovis18.tar
-rm endovis18.tar
-cd VOS-Endovis18/valid
-gdown --fuzzy "https://drive.google.com/file/d/1bDA8IwSxHIEGhcrcbsw9UsWMxnQmIIX2/view?usp=sharing"
-tar -xvf VOS-endovis18.tar
-rm VOS-endovis18.tar
+gdown --fuzzy "https://drive.google.com/file/d/1f3qdi7J2GsHGDmxdc4vXaf6ZsFGa_vOH/view?usp=sharing"
+tar -xvf endovis2018_new.tar
+rm endovis2018_new.tar
+# cd VOS-Endovis18/valid
+# gdown --fuzzy "https://drive.google.com/file/d/1bDA8IwSxHIEGhcrcbsw9UsWMxnQmIIX2/view?usp=sharing"
+# tar -xvf VOS-endovis18.tar
+# rm VOS-endovis18.tar
 cd ..
